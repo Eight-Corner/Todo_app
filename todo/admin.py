8e403943c2b19239eb0acc4f19da94da32fc8520
Eight-Corner <corner3499@gmail.com>
2022-04-23 21:47:49 +0900

    💿 DB 작업 & models 작업

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import Todo
+
+
+@admin.register(Todo) # 컨트롤 + 스페이스바 두번눌러서 제안
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'todo')
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    # 컬럼 =
+    name = models.CharField('NAME', max_length=5, blank=True) # blank는 빈값을 허용한다.
+    todo = models.CharField('TODO', max_length=50) # blank Default는 False 이다.
+
+    def _str_(self):
+        return self.todo
