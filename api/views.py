ef6042a559586e772d6434235801243e8e87ccb4
Eight-Corner <corner3499@gmail.com>
2022-04-27 23:27:29 +0900

    get JsonResponse

---
+++ api/views.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse
+from django.views.generic import ListView
+
+from todo.models import Todo
+
+
+class ApiTodoLV(ListView):
+    model = Todo
+    # template_name =
+
+    def get(self, request, *args, **kwargs):
+        tmpList = [
+            {'id': 1, 'name': 'd테스트1', 'todo': '테스트 내용 1'},
+            {'id': 2, 'name': 'd테스트4', 'todo': '테스트 내용 4'},
+            {'id': 3, 'name': 'd테스트2', 'todo': '테스트 내용 2'},
+            {'id': 4, 'name': 'd테스트3', 'todo': '테스트 내용 3'},
+        ]
+        return JsonResponse(data=tmpList, safe=False)
--- mysite/urls.py
+++ mysite/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
 
     path('', views.HomeView.as_view(), name='home'),
     path('todo/', include('todo.urls')),
+    path('api/', include('api.urls')),
 ]
