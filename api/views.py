965b9c651906f735aee3fef555500b84ea08d159
corner <corner3499@gmail.com>
2022-04-30 16:46:48 +0900

    todo create

--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
+import json
+
+from django.forms import model_to_dict
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic.edit import BaseDeleteView
+from django.views.generic.edit import BaseDeleteView, BaseCreateView
 from django.views.generic.list import BaseListView
 
 from todo.models import Todo
@@ -25,6 +28,8 @@ class ApiTodoLV(BaseListView):
     def render_to_response(self, context, **response_kwargs):
         todoList = list(context['object_list'].values())
         return JsonResponse(data=todoList, safe=False)
+
+
 #
 @method_decorator(csrf_exempt, name='dispatch')
 class ApiTodoDelV(BaseDeleteView):
@@ -34,3 +39,27 @@ class ApiTodoDelV(BaseDeleteView):
         self.object = self.get_object();
         self.object.delete()
         return JsonResponse(data={}, status=204)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ApiTodoCV(BaseCreateView):
+    model = Todo
+    fields = '__all__' # 필수
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['data'] = json.loads(self.request.body)
+        return kwargs
+
+    def form_valid(self, form):
+        # print("form_valid()", form)
+        self.object = form.save()
+        newTodo = model_to_dict(self.object)
+        # print(f"newtodo: {newTodo}") # f 스트링 파이썬 3.6부터 도입된 문법
+        return JsonResponse(data=newTodo, status=201)
+
+    def form_invalid(self, form):
+        # print("form_invaild()", form)
+        # print("form_invaild()", self.request.POST)
+        # print("form_invaild()", self.request.body.decode('utf8'))
+        return JsonResponse(data=form.errors, status=400) # 클라이언트 에러
