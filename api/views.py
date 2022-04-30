697a29ff307e7901840460a121cf012154d7666a
corner <corner3499@gmail.com>
2022-04-30 15:22:55 +0900

    Json Response & fix

--- api/views.py
+++ api/views.py
@@ -8,11 +8,17 @@ class ApiTodoLV(ListView):
     model = Todo
     # template_name =
 
-    def get(self, request, *args, **kwargs):
-        tmpList = [
-            {'id': 1, 'name': 'd테스트1', 'todo': '테스트 내용 1'},
-            {'id': 2, 'name': 'd테스트4', 'todo': '테스트 내용 4'},
-            {'id': 3, 'name': 'd테스트2', 'todo': '테스트 내용 2'},
-            {'id': 4, 'name': 'd테스트3', 'todo': '테스트 내용 3'},
-        ]
-        return JsonResponse(data=tmpList, safe=False)
+
+    # def get(self, request, *args, **kwargs):
+    #     tmpList = [
+    #         {'id': 1, 'name': 'd테스트1', 'todo': '테스트 내용 1'},
+    #         {'id': 2, 'name': 'd테스트4', 'todo': '테스트 내용 4'},
+    #         {'id': 3, 'name': 'd테스트2', 'todo': '테스트 내용 2'},
+    #         {'id': 4, 'name': 'd테스트3', 'todo': '테스트 내용 3'},
+    #     ]
+    #     return JsonResponse(data=tmpList, safe=False)
+    #
+
+    def render_to_response(self, context, **response_kwargs):
+        todoList = list(context['object_list'].values())
+        return JsonResponse(data=todoList, safe=False)
--- todo/models.py
+++ todo/models.py
@@ -5,12 +5,11 @@ class Todo(models.Model):
     name = models.CharField('NAME', max_length=5, blank=True)
     todo = models.CharField('TODO', max_length=50)
 
-    def _str_(self):
+    def __str__(self):
         return self.todo
 
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
         if not self.name:
             self.name = '홍길동'
-            super().save()
-
+        super().save()
